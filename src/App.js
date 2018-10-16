fc28313b647eb090a3048ef2ecced9e1875b4cf2
GuyBird <06gbird@gmail.com>
2018-10-16 10:41:56 +0100

    Test changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Component2 from './Component2';
+
 class App extends Component {
   render() {
     return (
@@ -11,14 +13,10 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React!!
           </a>
+          <Component2 />
         </header>
       </div>
     );
